9ebee9b3d8ebb71371d0a21f25fce838e47e4795
shadowrezi <urijozimko4@gmail.com>
2023-07-14 11:26:56 +0300

    It's First commit

--- src/main.rs
+++ src/main.rs
@@ -5,4 +5,11 @@ fn main() {
     let time = Local::now().format("%-I:%M");
 
     println!("{}", time);
+    println!("{}", test().unwrap());
+
+}
+
+
+fn test() -> Option<f64> {
+    return Some(3.141235123123211);
 }
