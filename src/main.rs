e0e7eae32bb557a67c9004aa6451832cf201ef9c
shadowrezi <urijozimko4@gmail.com>
2023-10-27 13:38:18 +0300

    Tests

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,44 @@
-use chrono::prelude::Local;
+use std::{
+    io::{
+        stdin,
+        BufReader
+    },
+    fs::File,
+    time::Duration,
+    thread::sleep
+};
+use rodio::{
+    Source,
+    Decoder
+};
+
 
 fn main() {
+    let mut x = "".to_string();
 
-    let time = Local::now().format("%-I:%M");
-
-    println!("{}", time);
-    println!("{}", test().unwrap());
-
+    loop {
+        match stdin().read_line(&mut x) {
+            Ok(_) => {},
+            Err(_) => {}
+        }
+        let time = x.trim().parse::<i32>().unwrap();
+    
+        timer(time);
+    }
 }
 
+fn timer(time: i32) {
+    for i in 0..=time {
+        println!("{} secs elapsed!", i);
 
-fn test() -> Option<f64> {
-    return Some(3.141235123123211);
+        sleep(Duration::from_secs(1));
+    }
+
+    //* Play audio ringtone
+    let (_stream, stream_handle) = rodio::OutputStream::try_default().unwrap();
+
+    let file = File::open("iphone_14.mp3").unwrap();
+    let source = Decoder::new(BufReader::new(file)).unwrap();
+    
+    let _ = stream_handle.play_raw(source.convert_samples());
 }
